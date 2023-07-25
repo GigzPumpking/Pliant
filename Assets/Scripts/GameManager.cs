b1ef499ec714921f2de8ce22c423840ec661af98
Dominic London <londonrdominic@gmail.com>
2023-07-25 14:13:52 -0700

    Game Manager started
    Game Manager has script for managing health and skeleton for managing position and spawn points.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] int health;
+    [SerializeField] static int startingHealth = 4;
+    [SerializeField] int health = startingHealth;
+    [SerializeField] GameObject player;
+    [SerializeField] Transform lastSpawnPoint;
+
+
 
     public void LoseHealth(int amount) => health -= amount;
 
     public void GainHealth(int amount) => health += amount;
+
+    private void Start()
+    {
+        NewGame();
+    }
+
+    private void Update()
+    {
+        if (health <= 0)
+            NewGame();
+    }
+
+    private void NewGame()
+    {
+        SetHealth(startingHealth);
+        ResetState();
+    }
+
+    private void ResetState()
+    {
+      //  player.transform = lastSpawnPoint;
+    }
+
+    private void SetHealth(int value)
+    {
+        health = startingHealth;
+    }
+
 }
