a7b57a061195a051614458f819b3eed90a79a4d2
Gigz <gigzpumpking@gmail.com>
2023-07-27 14:30:32 -0700

    Shadow + Frog Animation Update

--- Assets/Scripts/IsometricCharacterController.cs
+++ Assets/Scripts/IsometricCharacterController.cs
@@ -105,14 +105,11 @@ public class IsometricCharacterController : MonoBehaviour
             isMoving = true;
             if (direction == Direction.DOWN) {
                 animator.Play(jumpFrogDirections[0]);
-                // set scale of sprite to 0.6
-                TerrySprite.transform.localScale = new Vector3(0.6f, 0.6f, 1f);
-
             }
             else animator.Play(staticFrogDirections[1]);
         } else {
             timeElapsed += Time.deltaTime * movementSpeed / landDis;
-            if (timeElapsed <= 1.2f) {
+            if (timeElapsed <= 0.66667f) {
                 // turn off collision between player layer and world layer
                 Physics2D.IgnoreLayerCollision(6, 7, true);
                 currPos = Vector2.MoveTowards(currPos, landPos, Time.fixedDeltaTime*movementSpeed);
@@ -121,11 +118,8 @@ public class IsometricCharacterController : MonoBehaviour
                 if (landPos.y == jumpStartPos.y) shadow.transform.position = new Vector2(sprite.transform.position.x, jumpStartPos.y);
                 else {
                     shadow.transform.position = new Vector2(sprite.transform.position.x, sprite.transform.position.y - curveY.Evaluate(timeElapsed));
-                    
                 }
             } else {
-                // set back scale of sprite to 1
-                TerrySprite.transform.localScale = new Vector3(1f, 1f, 1f);
                 // turn on collision between player layer and world layer
                 Physics2D.IgnoreLayerCollision(6, 7, false);
                 jump = false;
