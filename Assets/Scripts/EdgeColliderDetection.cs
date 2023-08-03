63ec39bd6031e7499f506c2d0a75853b6e8e9772
Gigz <gigzpumpking@gmail.com>
2023-08-03 13:52:02 -0700

    Edge Collider work

--- Assets/Scripts/EdgeColliderDetection.cs
+++ Assets/Scripts/EdgeColliderDetection.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class EdgeColliderDetection : MonoBehaviour
 {
+    public Collider2D edgeCollider1;
+    public Collider2D edgeCollider2;
+    public Collider2D edgeCollider3;
+    public Collider2D edgeCollider4;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Debug.Log("edgeCollider1: " + edgeCollider1.IsTouchingLayers());
+        Debug.Log("edgeCollider2: " + edgeCollider2.IsTouchingLayers());
+        Debug.Log("edgeCollider3: " + edgeCollider3.IsTouchingLayers());
+        Debug.Log("edgeCollider4: " + edgeCollider4.IsTouchingLayers());
     }
 }
